1e37b1ea67244d63bfeea7034d4662180d43d7fe
Cata Buades <catabuades@gmail.com>
2017-08-21 23:57:57 +0200

    home page html and styles

---
+++ app.js
@@ -0,0 +1,25 @@
+// const express = require('express')
+// const path = require('path')
+// const bodyParser = require('body-parser')
+
+// const app = express()
+// // const routesApp = require('./routes')
+
+// const PORT = process.env.PORT || 3001
+
+// app.set('view engine', 'pug')
+// app.locals.pretty = true
+
+// app.use(express.static(path.join(__dirname, 'client')))
+
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+
+// app.use(routesApp)
+
+// app.get('/', (req, res) => {
+//   res.render('pages/home')
+// })
+
+// app.listen(PORT)
+// console.log(`Listening on PORT ${PORT}`)
