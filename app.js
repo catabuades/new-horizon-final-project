d492ce6c3ccd1fa0cd506410dcd6913747537e7c
Cata Buades <catabuades@gmail.com>
2017-08-23 16:22:20 +0200

    script start

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// const express = require('express')
-// const path = require('path')
-// const bodyParser = require('body-parser')
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
 
-// const app = express()
-// // const routesApp = require('./routes')
+const app = express()
+// const routesApp = require('./routes')
 
-// const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 
 // app.set('view engine', 'pug')
 // app.locals.pretty = true
 
-// app.use(express.static(path.join(__dirname, 'client')))
+app.use(express.static(path.join(__dirname, 'client')))
 
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-// // app.use(routesApp)
+// app.use(routesApp)
 
-// app.get('/search-results', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/views/pages/search-result.html'))
-// })
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/pages/home.html'))
+})
 
-// app.listen(PORT)
-// console.log(`Listening on PORT ${PORT}`)
+app.listen(PORT)
+console.log(`Listening on PORT ${PORT}`)
