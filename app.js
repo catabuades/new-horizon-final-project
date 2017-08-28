ccc755518dc4e25bae49cd8c416a52cf5f8285b3
Cata Buades <catabuades@gmail.com>
2017-08-28 11:17:36 +0200

    html to jade, and bower install

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express()
 
 const PORT = process.env.PORT || 3001
 
-// app.set('view engine', 'pug')
-// app.locals.pretty = true
+app.set('view engine', 'pug')
+app.locals.pretty = true
 
 app.use(express.static(path.join(__dirname, 'client')))
 
@@ -17,9 +17,41 @@ app.use(bodyParser.json())
 
 // app.use(routesApp)
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client/pages/home.html'))
-// })
+app.get('/', (req, res) => {
+  res.render('pages/home')
+})
+
+app.get('/search-result', (req, res) => {
+  res.render('pages/search-result')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('pages/contact')
+})
+
+app.get('/catalogue', (req, res) => {
+  res.render('pages/catalogue')
+})
+
+app.get('/booking-step1', (req, res) => {
+  res.render('pages/booking-step1')
+})
+
+app.get('/booking-step2', (req, res) => {
+  res.render('pages/booking-step2')
+})
+
+app.get('/booking-step3', (req, res) => {
+  res.render('pages/booking-step3')
+})
+
+app.get('/booking-step4', (req, res) => {
+  res.render('pages/booking-step4')
+})
+
+app.get('/bike-info', (req, res) => {
+  res.render('pages/bike-info')
+})
 
 app.listen(PORT)
 console.log(`Listening on PORT ${PORT}`)
