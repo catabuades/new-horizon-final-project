0b2c42cd52ae1e093ee417849c169cd73e8a8fc6
manuelbarzi <manuelbarzi@gmail.com>
2017-09-05 11:02:05 +0200

    correct bikes add-to-cart with jquery

--- client/js/main.js
+++ client/js/main.js
@@ -1,59 +1,72 @@
 console.log('Javascript ready to go...')
 
-const filterApplied = getFiltersApplied()
+var filterApplied = getFiltersApplied();
+
 filterApplied.forEach(filter => {
   $(`[data-on="${filter}"]`)
-        .bootstrapToggle('on')
+        .bootstrapToggle('on');
 })
 
 $('#road, #mountain, #trekking, #city, #e-bike').change(function () {
-  let filterApplied = getFiltersApplied()
-  const thisFilter = this.value
-  const isChecked = $(this).prop('checked')
-  console.log(isChecked)
-  if (isChecked) filterApplied.push(thisFilter)
-  else filterApplied = filterApplied.filter(filter => filter !== thisFilter)
-  const urlWithNewFilters = createQueryFilters(filterApplied)
-  window.location.href = urlWithNewFilters
-})
+  var filterApplied = getFiltersApplied();
+  var thisFilter = this.value;
+  var isChecked = $(this).prop('checked');
+  console.log(isChecked);
+  if (isChecked) filterApplied.push(thisFilter);
+  else filterApplied = filterApplied.filter(filter => filter !== thisFilter);
+  var urlWithNewFilters = createQueryFilters(filterApplied);
+  window.location.href = urlWithNewFilters;
+});
 
 function createQueryFilters (filters) {
-  const tplQueryFilters = '?filters='
-  const tplQueryFiltersSearch = '?filters='
-  let { search, origin, pathname } = window.location
+  var tplQueryFilters = '?filters=';
+  var tplQueryFiltersSearch = '?filters=';
+  var { search, origin, pathname } = window.location;
   if (pathname === '/catalogue') {
-    const newQuery = tplQueryFilters + filters.join(',')
-    return origin + pathname + newQuery
+    var newQuery = tplQueryFilters + filters.join(',');
+    return origin + pathname + newQuery;
   }
   if (pathname === '/apiResults') {
-    const newQuerySearch = tplQueryFiltersSearch + filters.join(',')
-    console.log(filters.join(','))
+    var newQuerySearch = tplQueryFiltersSearch + filters.join(',');
+    console.log(filters.join(','));
     // console.log(newQuerySearch)
     // return origin + pathname + newQuerySearch
   }
 }
 
 function getFiltersApplied () {
-  let { search, origin, pathname } = window.location
-  const tplQueryFilters = '?filters='
-  const aFilters = search.replace(tplQueryFilters, '').split(',')
-  return aFilters
+  var { search, origin, pathname } = window.location;
+  var tplQueryFilters = '?filters=';
+  var aFilters = search.replace(tplQueryFilters, '').split(',');
+  return aFilters;
 }
 
-// $('.addCart').on('click', function (e) {
-//   const bikeID = $(this).data('id')
-//   const size = $('.sizeToCard option:selected').text()
-//   console.log(size)
-//   const pedals = $('.pedalsToCard option:selected').text()
-//   console.log(pedals)
-//   const insurance = $('insuranceToCard').val()
-//   console.log(insurance)
-//   const url = `/cart/${bikeID}`
-//   const method = 'POST'
-//   const data = {size, pedals, insurance}
+$('.add-to-cart-form').submit(function(e) {
+  e.preventDefault();
 
-//   $.ajax({ url, method, data })
-//         .then(msg => {
-//           window.location.reload()
-//         })
-// })
+  var self = $(this);
+
+  var size = self.find('.size').val();
+
+  var pedals = self.find('.pedals').val();
+
+  var insurance = self.find('.insurance').is(':checked');
+
+  console.log(size, pedals, insurance);
+
+  // 
+  // var size = $('.sizeToCard option:selected').text();
+  // console.log(size);
+  // var pedals = $('.pedalsToCard option:selected').text();
+  // console.log(pedals);
+  // var insurance = $('insuranceToCard').val();
+  // console.log(insurance);
+  // var url = `/cart/${bikeId}`;
+  // var method = 'POST';
+  // var data = {size, pedals, insurance};
+
+  // $.ajax({ url, method, data })
+  //       .then(msg => {
+  //         window.location.reload()
+  //       });
+});
